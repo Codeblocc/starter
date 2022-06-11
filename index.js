b0ab77e82621d5ee0d0e3bb77bcd3c84810d211e
Abhishek <abhishekdhar1991@gmail.com>
2022-06-11 20:46:31 +0530

    createserver

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const fs = require("fs"); // fs stands for file system
+const http = require("http"); // this give sus the networking capability such as building an HTTP server
+
+//////////////////////////////////////////////////////////////
+// Files
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -8,17 +12,28 @@ const fs = require("fs"); // fs stands for file system
 // fs.writeFileSync("./txt/output.txt", textOut);
 // console.log("File written!");
 
-// Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// // Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile(".txt/final.txt", `#{data2}\n{data3}`, "utf-8", (err) => {
-        console.log("Your file has been written!");
-      });
-    });
-  });
+//       fs.writeFile(".txt/final.txt", `#{data2}\n{data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+//////////////////////////////////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on Port 8000");
+});
