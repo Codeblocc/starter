a061380502d4b8ddca06024d12ac7b8d128da2bc
Abhishek <abhishekdhar1991@gmail.com>
2022-06-12 12:24:31 +0530

    api

--- index.js
+++ index.js
@@ -38,10 +38,16 @@ const server = http.createServer((req, res) => {
     res.end("This is the Overview!");
   } else if (pathName === "/product") {
     res.end("This is the product!");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
   } else {
     res.writeHead(404, {
-      "Content-type": "text/gtml",
-      "my-own-header": "hellow-world",
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
     });
     res.end("<h1>Page not found!</h1>");
   }
